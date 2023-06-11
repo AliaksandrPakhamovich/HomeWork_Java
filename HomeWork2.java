b0824e9943340219c3e7377209f74949ce689dce
AliaksandrPakhamovich <133905306+AliaksandrPakhamovich@users.noreply.github.com>
2023-06-11 22:41:30 +0200

    HoweWork2

---
+++ HomeWork2.java
@@ -0,0 +1,22 @@
+public class HomeWork2 {
+    public static void main(String[] args) {
+        /*
+        Создайте переменные (любое имя) и определите типы (наиболее подходящие) для следующих выражений:
+         (float) Math.pow(Math.E, 5) + 1.5;
+        (byte) Math.log(2) + (short) 14;
+        1 + 1.5
+         */
+        double q ;
+        q = (float) Math.pow(Math.E, 5) + 1.5;
+        System.out.println(q);
+
+        int w;
+        w = (byte) Math.log(2) + (short)14;
+        System.out.println(w);
+
+        double e ;
+        e = 1 + 1.5;
+        System.out.println(e);
+
+    }
+}
